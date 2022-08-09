cd64e6b89ab5c963afa5c791d97485bca140f710
cambenassi <cambenassi@gmail.com>
2022-08-09 15:56:21 -0400

    heroku fixes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '/../client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+  });
+}
+
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from Express!" });
